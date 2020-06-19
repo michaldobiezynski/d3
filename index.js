68e461195b2fa2688a0ab2e4256fefcbbcec81c2
michaldobiezynski <dobiezynski@gmail.com>
2020-06-19 11:24:42 +0100

    joining Data to an Element

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-const canvas = d3.select(".canvas");
+data = [{ width: 200, height: 100, fill: "purple" }];
 
-const svg = canvas.append("svg").attr("height", 600).attr("width", 600);
+const svg = d3.select("svg");
 
-const group = svg.append("g").attr("transform", "translate(50, 120)");
+const rect = svg
+  .select("rect")
+  .data(data)
+  .attr("width", function (d, i, n) {
+    console.log(i);
+    console.log(n);
+    return d.width;
+  })
+  .attr("height", function (d) {
+    return d.height;
+  })
+  .attr("fill", function (d) {
+    return d.fill;
+  });
 
-group
-  .append("rect")
-  .attr("height", 100)
-  .attr("width", 200)
-  .attr("fill", "blue")
-  .attr("x", 20)
-  .attr("u", 20);
-
-group
-  .append("circle")
-  .attr("r", 50)
-  .attr("cx", 300)
-  .attr("cy", 70)
-  .attr("fill", "pink");
-
-group
-  .append("line")
-  .attr("x1", 370)
-  .attr("x2", 400)
-  .attr("y1", 20)
-  .attr("y2", 120)
-  .attr("stroke", "red");
-
-svg
-  .append("text")
-  .attr("x", 20)
-  .attr("y", 200)
-  .attr("fill", "grey")
-  .text("Hello, ninjas")
-  .style("font-family", "arial");
-
-console.log(canvas);
+console.log(rect);
