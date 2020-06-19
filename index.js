6bf550d74fb55d3c2dd56ea75870a57469bb2008
michaldobiezynski <dobiezynski@gmail.com>
2020-06-19 10:54:18 +0100

    method chaining & attributes

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const canvas = d3.select(".canvas");
 
-const svg = canvas.append("svg");
+const svg = canvas.append("svg").attr("height", 600).attr("width", 600);
 
-svg.append("rect");
-svg.append("circle");
-svg.append("line");
+svg
+  .append("rect")
+  .attr("height", 100)
+  .attr("width", 200)
+  .attr("fill", "blue")
+  .attr("x", 20)
+  .attr("u", 20);
+
+svg
+  .append("circle")
+  .attr("r", 50)
+  .attr("cx", 300)
+  .attr("cy", 70)
+  .attr("fill", "pink");
+
+svg
+  .append("line")
+  .attr("x1", 370)
+  .attr("x2", 400)
+  .attr("y1", 20)
+  .attr("y2", 120)
+  .attr("stroke", "red");
 
 console.log(canvas);
