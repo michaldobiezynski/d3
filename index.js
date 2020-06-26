00952c2f952d27f8c29303ad1319f8d327c57618
michaldobiezynski <dobiezynski@gmail.com>
2020-06-26 14:30:33 +0100

    creating an update function IMPORTANT

--- index.js
+++ index.js
@@ -15,18 +15,65 @@ const graph = svg
   .attr("height", graphHeight)
   .attr("transform", `translate(${margins.left}, ${margins.top})`);
 
-graph.append("rect");
-graph.append("rect");
-graph.append("rect");
-graph.append("rect");
-graph.append("rect");
-graph.append("rect");
-graph.append("rect");
-
 const xAxisGroup = graph
   .append("g")
   .attr("transform", `translate(0, ${graphHeight})`);
+
 const yAxisGroup = graph.append("g");
+
+//scales
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+const x = d3.scaleBand().range([0, 500]).paddingInner(0.2).paddingOuter(0.2);
+
+//create  the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(3)
+  .tickFormat((d) => d + " orders");
+
+//update x axis text
+xAxisGroup
+  .selectAll("text")
+  .attr("transform", "rotate(-40)")
+  .attr("text-anchor", "end")
+  .attr("fill", "orange");
+
+//update function
+const update = (data) => {
+  //updating scale domains
+  y.domain([0, d3.max(data, (d) => d.orders)]);
+  x.domain(data.map((item) => item.name));
+
+  // join the data to rects
+  const rects = graph.selectAll("rect").data(data);
+
+  //remove exit selection
+  rects.exit().remove();
+
+  //update current shapes in dom
+  rects
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("fill", "orange")
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.orders));
+
+  //append the enter selection to the DOM
+  rects
+    .enter()
+    .append("rect")
+    .attr("width", x.bandwidth)
+    .attr("height", (d) => graphHeight - y(d.orders))
+    .attr("fill", "orange")
+    .attr("x", (d) => x(d.name))
+    .attr("y", (d) => y(d.orders));
+
+  //call axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+};
 
 db.collection("dishes")
   .get()
@@ -36,53 +83,5 @@ db.collection("dishes")
       data.push(doc.data());
     });
 
-    console.log(data);
-    //creating the linear scale
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.orders)])
-      .range([graphHeight, 0]);
-    const x = d3
-      .scaleBand()
-      .domain(data.map((item) => item.name))
-      .range([0, 500])
-      .paddingInner(0.2)
-      .paddingOuter(0.2);
-
-    console.log(x("veg curry"));
-    console.log(x.bandwidth());
-
-    // join the data to rects
-    const rects = graph.selectAll("rect").data(data);
-
-    console.log(rects);
-
-    // rects
-    //   .attr("width", x.bandwidth)
-    //   .attr("height", (d) => graphHeight - y(d.orders))
-    //   .attr("fill", "orange")
-    //   .attr("x", (d) => x(d.name))
-    //   .attr("y", (d) => y(d.orders));
-    // //append the enter selection to the DOM
-    // rects
-    //   .enter()
-    //   .append("rect")
-    //   .attr("width", x.bandwidth)
-    //   .attr("height", (d) => graphHeight - y(d.orders))
-    //   .attr("fill", "orange")
-    //   .attr("x", (d) => x(d.name))
-    //   .attr("y", (d) => y(d.orders));
-    // //create and call the axes
-    // const xAxis = d3.axisBottom(x);
-    // const yAxis = d3
-    //   .axisLeft(y)
-    //   .ticks(3)
-    //   .tickFormat((d) => d + " orders");
-    // xAxisGroup.call(xAxis);
-    // yAxisGroup.call(yAxis);
-    // xAxisGroup
-    //   .selectAll("text")
-    //   .attr("transform", "rotate(-40)")
-    //   .attr("text-anchor", "end")
-    //   .attr("fill", "orange");
+    update(data);
   });
