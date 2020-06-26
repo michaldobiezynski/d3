a8569faaf6c8b0b61b4b99be8898dc50d177f36c
michaldobiezynski <dobiezynski@gmail.com>
2020-06-26 13:47:31 +0100

    setting up the firebase config and getting data from firestore

--- index.js
+++ index.js
@@ -20,56 +20,56 @@ const xAxisGroup = graph
   .attr("transform", `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append("g");
 
-d3.json("menu.json").then((data) => {
-  //creating the linear scale
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.orders)])
-    .range([graphHeight, 0]);
+db.collection("dishes")
+  .get()
+  .then((res) => {
+    let data = [];
+    res.docs.forEach((doc) => {
+      data.push(doc.data());
+    });
 
-  const x = d3
-    .scaleBand()
-    .domain(data.map((item) => item.name))
-    .range([0, 500])
-    .paddingInner(0.2)
-    .paddingOuter(0.2);
-
-  console.log(x("veg curry"));
-  console.log(x.bandwidth());
-
-  // join the data to rects
-  const rects = graph.selectAll("rect").data(data);
-
-  rects
-    .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
-    .attr("fill", "orange")
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders));
-
-  //append the enter selection to the DOM
-  rects
-    .enter()
-    .append("rect")
-    .attr("width", x.bandwidth)
-    .attr("height", (d) => graphHeight - y(d.orders))
-    .attr("fill", "orange")
-    .attr("x", (d) => x(d.name))
-    .attr("y", (d) => y(d.orders));
-
-  //create and call the axes
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3
-    .axisLeft(y)
-    .ticks(3)
-    .tickFormat((d) => d + " orders");
-
-  xAxisGroup.call(xAxis);
-  yAxisGroup.call(yAxis);
-
-  xAxisGroup
-    .selectAll("text")
-    .attr("transform", "rotate(-40)")
-    .attr("text-anchor", "end")
-    .attr("fill", "orange");
-});
+    console.log(data);
+    //creating the linear scale
+    const y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.orders)])
+      .range([graphHeight, 0]);
+    const x = d3
+      .scaleBand()
+      .domain(data.map((item) => item.name))
+      .range([0, 500])
+      .paddingInner(0.2)
+      .paddingOuter(0.2);
+    console.log(x("veg curry"));
+    console.log(x.bandwidth());
+    // join the data to rects
+    const rects = graph.selectAll("rect").data(data);
+    rects
+      .attr("width", x.bandwidth)
+      .attr("height", (d) => graphHeight - y(d.orders))
+      .attr("fill", "orange")
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.orders));
+    //append the enter selection to the DOM
+    rects
+      .enter()
+      .append("rect")
+      .attr("width", x.bandwidth)
+      .attr("height", (d) => graphHeight - y(d.orders))
+      .attr("fill", "orange")
+      .attr("x", (d) => x(d.name))
+      .attr("y", (d) => y(d.orders));
+    //create and call the axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3
+      .axisLeft(y)
+      .ticks(3)
+      .tickFormat((d) => d + " orders");
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
+    xAxisGroup
+      .selectAll("text")
+      .attr("transform", "rotate(-40)")
+      .attr("text-anchor", "end")
+      .attr("fill", "orange");
+  });
