59c97aa9b89de5468d2aae6f8c7106b442dacfd4
michaldobiezynski <dobiezynski@gmail.com>
2020-07-27 14:33:55 +0100

    arc enter tween

--- ninjaWonga donut chart/graph.js
+++ ninjaWonga donut chart/graph.js
@@ -41,10 +41,12 @@ const update = (data) => {
     .enter()
     .append("path")
     .attr("class", "arc")
-    .attr("d", arcPath)
     .attr("stroke", "#fff")
     .attr("stroke-width", 3)
-    .attr("fill", (d) => colour(d.data.name));
+    .attr("fill", (d) => colour(d.data.name))
+    .transition()
+    .duration(750)
+    .attrTween("d", arcTweenEnter);
 };
 
 // data array and firestore
@@ -72,3 +74,12 @@ db.collection("expenses").onSnapshot((res) => {
 
   update(data);
 });
+
+const arcTweenEnter = (d) => {
+  var i = d3.interpolate(d.endAngle, d.startAngle);
+
+  return function (t) {
+    d.startAngle = i(t);
+    return arcPath(d);
+  };
+};
