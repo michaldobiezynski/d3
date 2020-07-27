5e3bda34bcd9d8fb76b84a08fbbf519e2efbdc76
michaldobiezynski <dobiezynski@gmail.com>
2020-07-27 15:46:13 +0100

    adding legend

--- ninjaWonga donut chart/graph.js
+++ ninjaWonga donut chart/graph.js
@@ -1,6 +1,7 @@
 const dims = { height: 300, width: 300, radius: 150 };
 const cent = { x: dims.width / 2 + 5, y: dims.height / 2 + 5 };
 
+// create svg container
 const svg = d3
   .select(".canvas")
   .append("svg")
@@ -10,36 +11,50 @@ const svg = d3
 const graph = svg
   .append("g")
   .attr("transform", `translate(${cent.x}, ${cent.y})`);
+// translates the graph group to the middle of the svg container
 
 const pie = d3
   .pie()
   .sort(null)
   .value((d) => d.cost);
+// the value we are evaluating to create the pie angles
 
 const arcPath = d3
   .arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2);
 
+// ordinal colour scale
 const colour = d3.scaleOrdinal(d3["schemeSet3"]);
 
-//update function
-const update = (data) => {
-  //update colour scale domain
-  colour.domain((d) => d.name);
+// legend setup
+const legendGroup = svg
+  .append("g")
+  .attr("transform", `translate(${dims.width + 40}, 10)`);
 
-  //join enhanced (pie) data to path elements
+const legend = d3
+  .legendColor()
+  .shape("path", d3.symbol().type(d3.symbolCircle)())
+  .shapePadding(10)
+  .scale(colour);
+
+// update function
+const update = (data) => {
+  // update colour scale domain
+  colour.domain(data.map((d) => d.name));
+
+  // update legend
+  legendGroup.call(legend);
+  legendGroup.selectAll("text").attr("fill", "white");
+
+  // join enhanced (pie) data to path elements
   const paths = graph.selectAll("path").data(pie(data));
 
-  //remove exit selection
+  // handle the exit selection
   paths.exit().transition().duration(750).attrTween("d", arcTweenExit).remove();
 
-  //update current shapes in dom
-  paths
-    .attr("d", arcPath)
-    .transition()
-    .duration(750)
-    .attrTween("d", arcTweenUpdate);
+  // handle the current DOM path updates
+  paths.transition().duration(750).attrTween("d", arcTweenUpdate);
 
   paths
     .enter()
@@ -47,6 +62,7 @@ const update = (data) => {
     .attr("class", "arc")
     .attr("stroke", "#fff")
     .attr("stroke-width", 3)
+    .attr("d", arcPath)
     .attr("fill", (d) => colour(d.data.name))
     .each(function (d) {
       this._current = d;
@@ -59,31 +75,34 @@ const update = (data) => {
 // data array and firestore
 var data = [];
 
-db.collection("expenses").onSnapshot((res) => {
-  res.docChanges().forEach((change) => {
-    const doc = { ...change.doc.data(), id: change.doc.id };
+db.collection("expenses")
+  .orderBy("cost")
+  .onSnapshot((res) => {
+    res.docChanges().forEach((change) => {
+      const doc = { ...change.doc.data(), id: change.doc.id };
 
-    switch (change.type) {
-      case "added":
-        data.push(doc);
-        break;
-      case "modified":
-        const index = data.findIndex((item) => item.id == doc.id);
-        data[index] = doc;
-        break;
-      case "removed":
-        data = data.filter((item) => item.id !== doc.id);
-        break;
-      default:
-        break;
-    }
+      switch (change.type) {
+        case "added":
+          data.push(doc);
+          break;
+        case "modified":
+          const index = data.findIndex((item) => item.id == doc.id);
+          data[index] = doc;
+          break;
+        case "removed":
+          data = data.filter((item) => item.id !== doc.id);
+          break;
+        default:
+          break;
+      }
+    });
+
+    // call the update function
+    update(data);
   });
 
-  update(data);
-});
-
 const arcTweenEnter = (d) => {
-  var i = d3.interpolate(d.endAngle, d.startAngle);
+  var i = d3.interpolate(d.endAngle - 0.1, d.startAngle);
 
   return function (t) {
     d.startAngle = i(t);
@@ -102,13 +121,14 @@ const arcTweenExit = (d) => {
 
 // use function keyword to allow use of 'this'
 function arcTweenUpdate(d) {
-  //interpolate between the two objects
+  console.log(this._current, d);
+  // interpolate between the two objects
   var i = d3.interpolate(this._current, d);
-
-  //update the current prop with new updated data
-  this._current = d;
+  // update the current prop with new updated data
+  this._current = i(1);
 
   return function (t) {
+    // i(t) returns a value of d (data object) which we pass to arcPath
     return arcPath(i(t));
   };
 }
