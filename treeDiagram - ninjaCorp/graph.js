40f2a91c43cd2560cce666b07a22b8d3d25b8ecf
michaldobiezynski <dobiezynski@gmail.com>
2020-07-30 09:19:15 +0100

    graph setup

--- treeDiagram - ninjaCorp/graph.js
+++ treeDiagram - ninjaCorp/graph.js
@@ -1,4 +1,12 @@
-const dims
+const dims = { height: 500, width: 1100 };
+
+const svg = d3
+  .select(".canvas")
+  .append("svg")
+  .attr("width", dims.width + 100)
+  .attr("height", dims.height + 100);
+
+const graph = svg.append("g").attr("transform", "translate(50,50)");
 
 // data and firestore
 var data = [];
