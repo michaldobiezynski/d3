10c36a28e353f950f9695882a9b75b6f00de7baa
michaldobiezynski <dobiezynski@gmail.com>
2020-08-02 21:09:23 +0100

    add text for each node

--- treeDiagram - ninjaCorp/graph.js
+++ treeDiagram - ninjaCorp/graph.js
@@ -6,9 +6,9 @@ const svg = d3
   .attr("width", dims.width + 100)
   .attr("height", dims.height + 100);
 
-const graph = svg.append("g").attr("transform", "translate(50,50)");
+const graph = svg.append("g").attr("transform", "translate(50, 50)");
 
-// data strat
+// tree and stratify
 const stratify = d3
   .stratify()
   .id((d) => d.name)
@@ -16,38 +16,42 @@ const stratify = d3
 
 const tree = d3.tree().size([dims.width, dims.height]);
 
-//update function
+// update function
 const update = (data) => {
-  //get updated root node data
+  // remove current nodes
+  graph.selectAll(".node").remove();
+
+  // get updated root Node data
   const rootNode = stratify(data);
-  //   console.log(rootNode);
+  const treeData = tree(rootNode).descendants();
 
-  const treeData = tree(rootNode);
-  //   console.log(treeData);
+  // get nodes selection and join data
+  const nodes = graph.selectAll(".node").data(treeData);
 
-  //get nodes selection and join data
-  const nodes = graph.selectAll(".nodes").data(treeData.descendants());
-
-  //create enter node groups
+  // create enter node groups
   const enterNodes = nodes
     .enter()
     .append("g")
     .attr("class", "node")
-    .attr("transform", (d) => `translate(${d.x}, ${d.x})`);
+    .attr("transform", (d) => `translate(${d.x}, ${d.y})`);
 
-  //append rects to enter nodes
+  // append rects to enter nodes
   enterNodes
     .append("rect")
     .attr("fill", "#aaa")
     .attr("stroke", "#555")
     .attr("stroke-width", 2)
-    .attr("height", 50)
-    .attr("width", (d) => d.data.name.length * 20);
+    .attr("width", (d) => d.data.name.length * 20)
+    .attr("height", 50);
 
-  console.log(enterNodes);
+  enterNodes
+    .append("text")
+    .attr("text-anchor", "middle")
+    .attr("fill", "white")
+    .text((d) => d.data.name);
 };
 
-// data and firestore
+// data & firebase hook-up
 var data = [];
 
 db.collection("employees").onSnapshot((res) => {
